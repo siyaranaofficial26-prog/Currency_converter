2a43ffd6e2421187f4cc5ca997af105f76771d06
Siya Rana <siyaranaofficial26@gmail.com>
2026-06-17 19:45:14 +0530

    Refactor currency selection and update logic

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const BASE_URL= "https://fxapi.app/api";
 
 
-const button= document.querySelector("form button"); // Get the button element from the form
+const button= document.querySelector("form button"); 
 
-const fromCurrency=document.querySelector(".from select"); // Get the select element for the "from" currency
-const toCurrency=document.querySelector(".to select"); // Get the select element for the "to" currency
-const msg= document.querySelector(".msg"); // Get the element to display messages
-const dropdowns = document.querySelectorAll(".dropdown select"); // Get all select elements
-const btn= document.querySelector("button"); // Get the button element
+const fromCurrency=document.querySelector(".from select"); 
+const toCurrency=document.querySelector(".to select"); 
+const msg= document.querySelector(".msg"); 
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn= document.querySelector("button");
 const updateExchangeRate = async () => { 
 let amount= document.querySelector(".amount input");
     let amtVal= amount.value;
@@ -15,7 +15,7 @@ let amount= document.querySelector(".amount input");
         amtVal=1;
         amount.value= "1";
     }
-   // console.log(fromCurrency.value,toCurrency.value);
+  
    const URL = `${BASE_URL}/${fromCurrency.value.toLowerCase()}/${toCurrency.value.toLowerCase()}.json`;
 
    let response= await fetch(URL);
@@ -26,19 +26,18 @@ console.log(rate);
 msg.innerText= `${amtVal} ${fromCurrency.value} = ${finalAmt} ${toCurrency.value}`;
 }
 
-for(let select of dropdowns){ // dropdowns is a nodelist of select tags
-    for(let currency_code in countryList){ // countryList is an object with currency codes as keys and country codes as values
+for(let select of dropdowns){ 
+    for(let currency_code in countryList){ 
         let option = document.createElement("option");
-        option.value = currency_code;  // Set the value of the option to the currency code
-        option.innerText = currency_code; // Set the text of the option to the currency code
-        
+        option.value = currency_code; 
+        option.innerText = currency_code; 
         if(select.name == "from" && currency_code == "USD"){
-            option.selected = true; // Set USD as the default selected option for the "from" select element
+            option.selected = true; 
         }   
         if(select.name == "to" && currency_code == "INR"){
-            option.selected = true; // Set INR as the default selected option for the "to" select element
+            option.selected = true; 
         }
-        select.append(option); // Append the option to the select element
+        select.append(option);
         
     }
     select.addEventListener("change", (evt)=>{
@@ -55,9 +54,9 @@ const updateFlag= (element) =>{
 }
 
 btn.addEventListener("click", (evt)=>{
-    evt.preventDefault(); // Prevent the default form submission that takes place when the button is clicked
-    updateExchangeRate(); // Call the function to update the exchange rate
+    evt.preventDefault(); 
+    updateExchangeRate(); 
 })
 window.addEventListener("load", ()=>{
-    updateExchangeRate(); // Call the function to update the exchange rate when the page loads
+    updateExchangeRate(); 
 });
